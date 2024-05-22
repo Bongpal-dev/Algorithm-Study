6947fba787873cda156bde651d886f002e7c2084
YS <etgt777@naver.com>
2024-05-22 11:32:35 +0900

    selection sort

--- app/src/main/java/com/bongpal/algorithmstudy/sort/selection/SelectionSort.kt
+++ app/src/main/java/com/bongpal/algorithmstudy/sort/selection/SelectionSort.kt
@@ -1,2 +1,26 @@
 package com.bongpal.algorithmstudy.sort.selection
 
+fun main() {
+    val insert = intArrayOf(5, 2, 1, 9, 6, 3, 7, 4, 10, 8).selectionSort()
+
+    println(insert.joinToString())
+}
+
+fun IntArray.selectionSort(): IntArray {
+    var minIndex = 0
+
+    for (i in 0 .. this.lastIndex) {
+        minIndex = i
+
+        for (j in i + 1 .. this.lastIndex) {
+            if (this[j] < this[minIndex]) {
+                minIndex = j
+            }
+        }
+        val temp = this[minIndex]
+
+        this[minIndex] = this[i]
+        this[i] = temp
+    }
+    return this.copyOf()
+}
