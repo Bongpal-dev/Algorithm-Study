ad0bc1f9d6d1671598f8316e9976c33e3825491f
YS <etgt777@naver.com>
2024-06-05 11:04:59 +0900

    q.7 two sum

---
+++ app/src/main/java/com/bongpal/algorithmstudy/array/Q_7_Two_Sum.kt
@@ -0,0 +1,27 @@
+package com.bongpal.algorithmstudy.array
+
+import androidx.core.text.isDigitsOnly
+
+fun main() {
+    val sol = Solution7()
+
+    println(sol.twoSum(intArrayOf(2, 7, 11, 15), 9).joinToString())
+    println(sol.twoSum(intArrayOf(3, 2, 4), 6).joinToString())
+    println(sol.twoSum(intArrayOf(3, 3), 6).joinToString())
+    println(sol.twoSum(intArrayOf(-3, 4, 3, 90), 0).joinToString())
+}
+
+class Solution7 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        loop@ for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] + nums[j] == target) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        return intArrayOf()
+    }
+}
+
+// https://leetcode.com/problems/two-sum/
